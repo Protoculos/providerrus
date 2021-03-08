12e759fc8dd17fc0940be59dda436b8b579f2c06
Protoculos <mazdak2012@gmail.com>
2021-03-09 01:33:21 +0300

    Blank Lifting Up

---
+++ android/app/src/main/kotlin/com/experimentapp/provider_rus/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.experimentapp.provider_rus
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          child: Text('data'),
+        ),
+      ),
+      body: Center(
+        child: Widget1(),
+      ),
+    );
+  }
+}
+
+class Widget1 extends StatelessWidget {
+  Widget1();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Widget2(),
+    );
+  }
+}
+
+class Widget2 extends StatelessWidget {
+  Widget2();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            child: Text('Widget2'),
+          ),
+          Widget3(),
+        ],
+      ),
+    );
+  }
+}
+
+class Widget3 extends StatelessWidget {
+  Widget3();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('data'),
+    );
+  }
+}
